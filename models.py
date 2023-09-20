0dfc936abc72ddb38b7e1893eff5777896f5fb9b
Rob Craig <rob.lockhart@yahoo.co.uk>
2023-09-20 08:59:32 +0100

    working

--- models.py
+++ models.py
@@ -1,1 +1,19 @@
-from sqlalchemy import Boo;ean
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from database import Base
+
+class Questions(Base):
+  __tablename__ = 'questions'
+  
+  id = Column(Integer, primary_key=True, index=True)
+  question_text = Column(String, index=True)
+
+class Choices(Base):
+  __tablename__ = 'choices'
+  
+  id = Column(Integer, primary_key=True, index=True)
+  choice_text = Column(String, index=True)
+  is_correct = Column(Boolean, default=False)
+  choice_text = Column(String, index=True)
+  question_id = Column(Integer, ForeignKey("questions.id"))
+  
+  
